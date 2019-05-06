d080563b141a5c36605f46976e746973d186ddc3
Samy Ayoub <samyayoub@gmail.com>
2019-05-06 05:24:02 +0000

    Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :require_enrolled_user_for_current_course, only: [:show]
 
     def show
 
@@ -6,8 +8,15 @@ class LessonsController < ApplicationController
 
     private
 
+    def require_enrolled_user_for_current_course
+        if current_user.enrolled_in?(current_lesson.section.course) == false
+            redirect_to course_path(current_lesson.section.course), alert: 'You need to enroll for the course to view all the lessons and start learning!'
+        end
+    end
+
     helper_method :current_lesson
     def current_lesson
         @current_lesson ||= Lesson.find(params[:id])
     end
+
 end
